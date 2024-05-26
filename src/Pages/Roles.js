c87b73d78f458f1e3175b91fcb31207de2d88a8d
Adisagar J <contactadhi2203@gmail.com>
2024-05-26 22:46:12 +0530

    UI Enhancement and Static panel added. Completed the Role View Page.

--- src/Pages/Roles.js
+++ src/Pages/Roles.js
@@ -1,13 +1,54 @@
-// src/components/Roles.js
 import React from 'react';
+import { Table, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import './Roles.css';
+import SearchBar from './SearchBar';
+
+const columns = [
+  {
+    title: 'Role Name',
+    dataIndex: 'roleName',
+  },
+  {
+    title: 'Action',
+    dataIndex: 'action',
+    render: (text, record) => (
+      <div className="horizontal-buttons">
+        <Button type="primary" style={{ marginRight: 8 }}>Show</Button>
+        <Button type="default" style={{ marginRight: 8 }}>Edit</Button>
+        <Button type="danger">Delete</Button>
+      </div>
+    ),
+  },
+];
+
+const data = [
+  { key: '1', roleName: 'Commissioner', },
+  { key: '2', roleName: 'Customer', },
+  { key: '3', roleName: 'Sanitary Inspector', },
+  { key: '4', roleName: 'Staff', },
+  { key: '5', roleName: 'Supervisor', },
+];
+
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
 
 function Roles() {
-    return (
-        <div>
-            <h2>Roles</h2>
-            <p>Content for Roles</p>
+  return (
+    <div className="roles-container">
+      <div className="roles-header">
+      <h2>View Roles</h2>
+        <div style={{ display: 'flex' }}>
+          <Button type="primary" icon={<PlusOutlined />} style={{ marginLeft: 'auto', marginRight: 16 }}>
+            Create New Role
+          </Button>
         </div>
-    );
+      </div>      
+      <SearchBar/>
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+    </div>
+  );
 }
 
 export default Roles;
---
+++ src/Pages/SearchBar.js
@@ -0,0 +1,10 @@
+import { Input } from 'antd';
+
+const SearchBar = () => {
+    return (
+      <Input.Search placeholder="Type in to Search" />
+    );
+  };
+  
+  export default SearchBar;
+  
