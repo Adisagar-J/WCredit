c18550f372fac1b5414c0ae825f8336fc78f0ad6
Adisagar J <contactadhi2203@gmail.com>
2024-05-26 18:43:04 +0530

    Pages Folder added for Roles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './SignIn';
-import Dashboard from './Dashboard';
+import Dashboard from '../src/Pages/Dashboard';
+import Roles from '../src/Pages/Roles';
 
 
-function App()
-{
-  return(
+function App() {
+  return (
     <Router>
       <Routes>
-        <Route path='/' element={<SignIn/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/' element={<SignIn />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/roles" Pages={Roles} />
+        {/* <Route path="/users" Pages={Users} />
+        <Route path="/add-wardmember" Pages={AddWardMember} />
+        <Route path="/vehicle-master" Pages={VehicleMaster} />
+        <Route path="/master-ward" Pages={MasterWard} />
+        <Route path="/assign-staff-vehicle" Pages={AssignStaffVehicle} />
+        <Route path="/master-points" Pages={MasterPoints} />
+        <Route path="/location-tracking" Pages={LocationTracking} /> */}
       </Routes>
     </Router>
   );
